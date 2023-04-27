5a90b730b72ef18898668c505810ac07d6ad9efc
AndLego <galeon.gb@gmail.com>
2023-04-26 21:01:20 -0500

    planing the layout

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="background">
+        <div className="triangle_left"></div>
+        <div className="triangle_right"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
